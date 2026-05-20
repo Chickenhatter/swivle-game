895cf85021240cf73086c849ff85a80e7fe069bf
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-05-20 21:52:45 +1200

    1hr commit, I made what end of game works and started work on adding collision

--- code/character_run.gd
+++ code/character_run.gd
@@ -1,16 +1,29 @@
 extends CharacterBody2D
+var playing = true
 var a = 0
 var speed = 0
+var meters = 0
 func _physics_process(delta: float) -> void:
-	speed += 0.001                     
-	$".".position.x += (0.7+speed)
-	$"../Camera2D".position.x += (0.7+speed)
-	if Input.is_action_pressed('ui_space'):
-		if a < 0:
-			a += 0.08                 
-		a += 0.05                            
-	elif a > 0:
-		a -= 0.08
-	a -= 0.025
-	$".".position.y -= a
+	if playing == true:
+		meters += 0.01
+		$"."  .rotation_degrees = -a*20             
+		speed += 0.001                     
+		$".".position.x += (0.7+speed)
+		$"../Camera2D".position.x += (0.7+speed)
+		if Input.is_action_pressed('ui_space'):
+			if a < 0:
+				a += 0.08                 
+			a += 0.05                            
+		elif a        > 0:
+			a                   -= 0.08
+		a -= 0.025
+		$".".position.y -= a
 	move_and_slide()
+
+
+func _on_character_loss_find_area_entered(area: Area2D) -> void:
+	if area.name == 'loss':
+		playing = false
+		Global.meters = meters
+		await get_tree().create_timer(3.0).timeout
+		get_tree().change_scene_to_file("res://scenes/ending.tscn")
---
+++ code/ending.gd
@@ -0,0 +1,17 @@
+extends Node2D
+var grade = ''
+func _ready() -> void:
+	var meter = str(Global.meters)
+	grade = 'Not Achieved'
+	if Global.meters > 50:
+		grade = 'Achieved'
+	if Global.meters > 70:
+		grade = 'Merit'
+	if Global.meters > 100:
+		grade = 'Excellence'
+	$RichTextLabel.text = meter + 'meters traveled, you have now gotten a' + grade
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
