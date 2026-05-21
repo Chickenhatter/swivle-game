fc19874c1a351c6e174aef8580562eb447d46a2b
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-05-21 19:14:49 +1200

    1.75hr was working on making scene

--- code/ending.gd
+++ code/ending.gd
@@ -1,7 +1,7 @@
 extends Node2D
 var grade = ''
 func _ready() -> void:
-	var meter = str(Global.meters)
+	var meter = str(("%0.1f" %(Global.meters)))
 	grade = 'Not Achieved'
 	if Global.meters > 50:
 		grade = 'Achieved'
@@ -9,7 +9,7 @@ func _ready() -> void:
 		grade = 'Merit'
 	if Global.meters > 100:
 		grade = 'Excellence'
-	$RichTextLabel.text = meter + 'meters traveled, you have now gotten a' + grade
+	$RichTextLabel.text = meter + ' meters traveled, you have now gotten a ' + grade
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
