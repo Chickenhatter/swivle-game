aa3b7a15d20abdaabdafc1e7d2c9416e8f6a26f6
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-05-21 21:32:08 +1200

    2.5hr started on start

--- code/character_run.gd
+++ code/character_run.gd
@@ -5,19 +5,22 @@ var speed = 0
 var meters = 0
 func _physics_process(delta: float) -> void:
 	if playing == true:
-		meters += 0.015    
+		meters += 0.025
 		$"."  .rotation_degrees = -a*20             
-		speed += 0.0035          
+		speed += 0.001
 		$".".position.x += (0.7+speed)
 		$"../Camera2D".position.x += (0.7+speed)
 		if Input.is_action_pressed('ui_space'):
 			if a < 0:
 				a += 0.08                 
-			a += 0.05                            
+			a += 0.06        
 		elif a        > 0:
 			a                   -= 0.08
-		a -= 0.025
+		a -= 0.03
 		$".".position.y -= a
+	if $".".position.x >= 4868.0:
+		$".".position.x  = -200
+		$"../Camera2D".position.x = -200
 	move_and_slide()
 
 
---
+++ code/start_character.gd
@@ -0,0 +1,19 @@
+extends Node2D
+var dropping = false
+var a = 0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if dropping == true:
+		a -= 0.002
+		$Sprite2D.rotation_degrees = -a*200
+		$Sprite2D.position.y += -a*10
+		$Sprite2D.position.x += 1
+
+
+func _on_button_pressed() -> void:
+	dropping = true
